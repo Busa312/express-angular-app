9cdf6b99954bb43128523e82aabc7eadcc17744e
Busa312 <besarionigadelia@gmail.com>
2022-09-06 13:57:05 +0400

    getting ready to deploy

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
+const express = require("express");
+const compression = require("compression");
+
+const _port = 4100;
+const _app_folder = 'dist/personal-project-angular';
+
+const app = express();
+app.use(compression());
+
+
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
